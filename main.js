c24c423ee67a2c4c58f2d5650b84cd2161b94d61
TomerNoy <mail2tomer@gmail.com>
2021-11-14 10:35:43 +0200

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+var links = document.getElementById('links').children;
+for (l of links) {
+    l.addEventListener('click', (e) => { 
+        console.log(e.target)
+    })
+
+}
+
+// var btns = header.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   if (current.length > 0) { 
+//     current[0].className = current[0].className.replace(" active", "");
+//   }
+//   this.className += " active";
+//   });
+// }
